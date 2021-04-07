7be7a0a118e0a862016f0ca792841320ccd21f77
João Vitor <vitorpeu@hotmail.com>
2021-04-06 22:18:49 -0300

    Refactor

--- main.R
+++ main.R
@@ -6,11 +6,13 @@ library(tseries)
 library(ggplot2)
 library(astsa)
 library(expand)
+
 #csv to ts
-dt <- read.csv(file="SM.csv", header=TRUE, sep=",")
+dt <- read.csv(file="./csv/SM.csv", header=TRUE, sep=",")
 ts <- ts(dt$data,frequency=4)
 treino=window(ts,start=c(1,1),end=c(12,1))
 teste=window(ts,start=c(12,2),end=c(14,1))
+
 #testes de estacionaridade  
 print(adf.test(treino))
 111111
@@ -26,6 +28,7 @@ treinoDiff = diff(treinoDiff)
 acf(treinoDiff)
 treinoDiff = diff(treinoDiff)
 acf(treinoDiff)
+
 #mudanca do FAC 4, ordem de dff igual a frequencia
 treinoDiff = diff(treinoDiff)
 acf(treinoDiff)
@@ -38,15 +41,16 @@ tsDiff = diff(tsDiff)
 acf(tsDiff)
 tsDiff = diff(tsDiff)
 acf(tsDiff)
+
 #mudanca do FAC 4, ordem de dff igual a frequencia
 tsDiff = diff(tsDiff)
 acf(tsDiff)
+
 #tests
 resultados = data.frame("p"=numeric(),"d"=numeric(),"q"=numeric(),"P"=numeric(),"D"=numeric(),"Q"=numeric(), "RMSE"=character(), "AICC"=character())
 combinacoes = expand.grid(rep(list(0:5), 4))
+
 for (i in 1:nrow(combinacoes)) {
-  
-  
   row = y[i,]
   modelo = tryCatch({
     
@@ -60,10 +64,7 @@ for (i in 1:nrow(combinacoes)) {
     P = row$Var3
     D = 1
     Q = row$Var4
-    
-    
-    
-    
+ 
     RMSE = toString(sqrt(MSE))
     
     AICC = toString(modelo$aicc)
@@ -72,16 +73,11 @@ for (i in 1:nrow(combinacoes)) {
     
     resultados = rbind(resultados,linha)
     
-    
   }, warning = function(warning_condition) {}, error = function(error_condition) {}, finally={})
-  
-  #resultados = rbind(resultados,list(p,d,q,P,D,Q,RMSE,AICC))
-
 
 }
 
 print(resultados)
-
 
 resultados1 = resultados[3:4,]
 resultados2 = resultados[9:10,]
@@ -103,14 +99,10 @@ resultadoFinal = rbind(resultadoFinal,resultados9)
 
 print(resultadoFinal)
 
-write.csv(resultadoFinal,"MelhoresModelosByAICC.csv")
-
+write.csv(resultadoFinal,"./csv/MelhoresModelosByAICC.csv")
 
 for (i in 1:nrow(resultadoFinal)) {
-  
-  
   row = resultadoFinal[i,]
-  
   
   modelo = Arima(treino, order=c(row[[1]],row[[2]],row[[3]]), seasonal=list(order = c(row[[4]],row[[5]],row[[6]]), period = 4) , method = 'ML')
   prev =forecast(modelo,h=8)
@@ -120,24 +112,26 @@ for (i in 1:nrow(resultadoFinal)) {
   print(shapiro.test(modelo$residuals))
   
 }
+
 get30porcent = function(x){
-  
   y = (30*x)/100
   return(-y)
-  
+
 }
+
 modelo = Arima(treino, order=c(0,1,1), seasonal=list(order = c(0,1,0), period = 4) , method = 'ML')
 prev =forecast(modelo,h=8)
 
 for (i in prev$mean){
-  
   cdf = ecdf(sample(modelo$residuals,1000,replace = T))
   cdf2 = ecdf(sample(modelo$residuals+4500,1000,replace = T))
+
   print(i)
   print(cdf(get30porcent(i))*100)#probablidade da produção cair 30% do previsto
   print(cdf2(i)*100)#probabilidade de funcionamento perfeito
 
 }
+
 #bias
 #residuals
 #Decomposicao
